9e74af07b1486584059ed4727e77c5585edbbdda
manav <manavjotsingh17@gmail.com>
2026-07-20 20:36:57 -0700

    update port

--- server/main.py
+++ server/main.py
@@ -15,7 +15,7 @@ app = create_app()
 def main() -> None:
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
 
 
 if __name__ == "__main__":
